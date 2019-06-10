04a29b3f6d5cce4d2a8bfdb180e6afc2fd805861
yuzo <yuzopro@gmail.com>
2019-06-10 11:38:01 +0800

    增加免责声明

--- lib/page/drawer_page.dart
+++ lib/page/drawer_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:open_git/bean/user_bean.dart';
-import 'package:open_git/common/shared_prf_key.dart';
 import 'package:open_git/localizations/app_localizations.dart';
 import 'package:open_git/manager/login_manager.dart';
 import 'package:open_git/route/navigator_util.dart';
 import 'package:open_git/util/image_util.dart';
-import 'package:open_git/util/shared_prf_util.dart';
 
 class DrawerPage extends StatelessWidget {
   final String name, email, headUrl;
@@ -42,13 +40,13 @@ class DrawerPage extends StatelessWidget {
               NavigatorUtil.goTrend(context, "all");
             },
           ),
-          new ListTile(
-            title: new Text(AppLocalizations.of(context).currentlocal.bookmark),
-            leading: new Icon(Icons.bookmark, color: Colors.grey),
-            onTap: () {
-              NavigatorUtil.goBookMark(context);
-            },
-          ),
+//          new ListTile(
+//            title: new Text(AppLocalizations.of(context).currentlocal.bookmark),
+//            leading: new Icon(Icons.bookmark, color: Colors.grey),
+//            onTap: () {
+//              NavigatorUtil.goBookMark(context);
+//            },
+//          ),
           new ListTile(
             title: new Text(AppLocalizations.of(context).currentlocal.setting),
             leading: new Icon(Icons.settings, color: Colors.grey),
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -6,6 +6,17 @@ import 'package:open_git/route/navigator_util.dart';
 import 'package:open_git/util/image_util.dart';
 import 'package:open_git/widget/pull_refresh_list.dart';
 
+const disclaimerText1 = '本APP属于个人的非赢利性开源项目，以供开源社区使用，凡本APP转载的所有的文章 、'
+    '图片、音频、视频文件等资料的版权归版权所有人所有，本APP采用的非本站原创文章及'
+    '图片等内容无法一一和版权者联系，如果本网所选内容的文章作者及编辑认为其作品不宜'
+    '上网供大家浏览，或不应无偿使用请及时用电子邮件或电话通知我们，以迅速采取适当措施，'
+    '避免给双方造成不必要的经济损失。';
+
+const disclaimerText2 = '对于已经授权本APP独家使用并提供给本站资料的版权所有人的文章、'
+    '图片等资料，如需转载使用，需取得本站和版权所有人的同意。本APP所刊发、转载的文章，其版权均归原'
+    '作者所有，如其他媒体、网站或个人从本网下载使用，请在转载有关文章时务必尊重该文章的著作权，'
+    '保留本网注明的“稿件来源”，并自负版权等法律责任。';
+
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -26,6 +37,21 @@ class _HomePageState
     super.build(context);
     return new Scaffold(
       body: buildBody(context),
+      floatingActionButton: buildFloatingActionButton(),
+    );
+  }
+
+  @override
+  Widget buildFloatingActionButton() {
+    return new FloatingActionButton(
+      onPressed: () {
+        showAlertDialog(context);
+      },
+      child: new Text(
+        '免责\n声明',
+        style: new TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Theme.of(context).primaryColor,
     );
   }
 
@@ -60,11 +86,9 @@ class _HomePageState
               Row(
                 children: <Widget>[
                   _getItemBottom(
-                      'image/ic_like.png',
-                      item.collectionCount.toString()),
+                      'image/ic_like.png', item.collectionCount.toString()),
                   _getItemBottom(
-                      'image/ic_comment.png',
-                      item.commentsCount.toString()),
+                      'image/ic_comment.png', item.commentsCount.toString()),
                 ],
               ),
             ],
@@ -121,14 +145,14 @@ class _HomePageState
       padding: new EdgeInsets.only(right: 12.0),
       child: Row(
         children: <Widget>[
-          Image(
-              width: 12.0,
-              height: 12.0,
-              image: new AssetImage(icon)),
-          Padding(padding: EdgeInsets.only(left: 3.0), child: Text(
-            count,
-            style: new TextStyle(color: Colors.grey, fontSize: 12.0),
-          ),),
+          Image(width: 12.0, height: 12.0, image: new AssetImage(icon)),
+          Padding(
+            padding: EdgeInsets.only(left: 3.0),
+            child: Text(
+              count,
+              style: new TextStyle(color: Colors.grey, fontSize: 12.0),
+            ),
+          ),
         ],
       ),
     );
@@ -144,6 +168,52 @@ class _HomePageState
     return new Text(
       tag,
       style: new TextStyle(color: Colors.grey, fontSize: 12.0),
+    );
+  }
+
+  void showAlertDialog(BuildContext context) {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('免责声明',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700)),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(disclaimerText1),
+                Text(disclaimerText2),
+              ],
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(20.0)), // 圆角
+
+          actions: <Widget>[
+            new Container(
+              width: 250,
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      '知道了',
+                      style: TextStyle(fontSize: 16.0, color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  )
+                ],
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
