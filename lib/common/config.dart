9886a26d31bc7620d8b31b4e49056c9356a2cd33
yuzo <yuzopro@gmail.com>
2019-06-12 16:45:24 +0800

    修改github appid

--- lib/common/config.dart
+++ lib/common/config.dart
@@ -1,5 +1,5 @@
 class Config {
   static const int PAGE_SIZE = 20;
-  static const CLIENT_ID = "1d1d0f0e84625e416efb";
-  static const CLIENT_SECRET = "d8cb03c0f6dc85ebf610077148b0471aa66f1b42";
+  static const CLIENT_ID = "e7296dc864677f8c7ddb";
+  static const CLIENT_SECRET = "37bee0f576c29767a17d69a40830d7c243115fd0";
 }
